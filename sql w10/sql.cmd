9a1f884505b94ff8e94a2b5b2db52dc912f2dd95
dmsergeevich63 <47436708+dmsergeevich63@users.noreply.github.com>
2023-11-20 00:09:34 +0400

    Add files via upload

---
+++ sql w10/sql.cmd
@@ -0,0 +1,6 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'sql.exe' with Python DLL being found.
+set PATH=c:\users\novik\appdata\local\programs\python\PYTHON~1;%PATH%
+set PYTHONHOME=C:\Users\novik\AppData\Local\Programs\Python\Python311
+"%~dp0sql.exe" %*
